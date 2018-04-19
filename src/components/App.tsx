275cc700441715ed4d5195ffe191f3418dfaa401
yhlben <yinhengli@sina.com>
2018-04-19 18:09:34 +0800

    init project

---
+++ src/components/App.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+class App extends React.Component {
+  public render() {
+    return (<div>56123452223888</div>);
+  }
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  mode: "development",
+
+  entry: "./src/index",
+
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "name-[hash:8].js"
+  },
+  module: {
+    rules: [{
+      test: /.tsx?$/,
+      loader: 'babel-loader!ts-loader'
+    }]
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".jsx", ".js", ".json"]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Custom template',
+      template: 'index.html'
+    }),
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  devServer: {
+    hot: true,
+    inline: true
+  }
+};
